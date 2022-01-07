e52c7517d1d9d56d6f9dbf3630883f1adc0c1d10
angelica <angelica@amsbbgroup.com>
2022-01-07 09:42:20 +0800

    remove unnecessary codes

--- src/App.js
+++ src/App.js
@@ -18,43 +18,6 @@ const searchClient = algoliasearch(
 );
 
 function App() {
-  const [newEventName, setNewEventName] = useState("");
-  const [newEventInfo, setNewEventInfo] = useState("");
-
-  const [events, setEvents] = useState([]);
-  const eventsCollectionRef = collection(db, "events");
-
-  const createEvent = async () => {
-    await addDoc(eventsCollectionRef, {
-      eventName: newEventName,
-      eventInfo: newEventInfo,
-    });
-  };
-
-  const updateEvent = async (id) => {
-    const eventDoc = doc(db, "events", id);
-    const newFields = { eventName: newEventName, eventInfo: newEventInfo };
-    await updateDoc(eventDoc, newFields);
-  };
-
-  const deleteEvent = async (id) => {
-    const eventDoc = doc(db, "events", id);
-    await deleteDoc(eventDoc);
-  };
-
-  useEffect(() => {
-    const getEvents = async () => {
-      const data = await getDocs(eventsCollectionRef);
-      setEvents(
-        data.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }))
-      );
-    };
-    getEvents();
-  }, []);
-
   const Hit = ({ hit }: { hit: events }) => {
     return (
       <section class="main">
@@ -71,48 +34,15 @@ function App() {
 
   return (
     <div className="App">
-      {/* <input
-        placeholder="Name"
-        onChange={(event) => {
-          setNewEventName(event.target.value);
-        }}
-      />
-      &nbsp;
-      <input
-        placeholder="Info"
-        onChange={(event) => {
-          setNewEventInfo(event.target.value);
-        }}
-      /> */}
-      {/* <button onClick={createEvent}>Create Event</button> */}
-      {/* {events.map((event) => {
-        return ( */}
       <div>
         <InstantSearch searchClient={searchClient} indexName="events">
           <div className="search">
             <SearchBox />
           </div>
           <br />
-          {/* <h1>Events: {event.eventName}</h1>
-              <h1>Info: {event.eventInfo}</h1> */}
-          {/* <button
-                onClick={() => {
-                  updateEvent(event.id);
-                }}
-              >
-                Update
-              </button> */}
-          {/* <button
-            onClick={() => {
-              deleteEvent(event.id);
-            }}
-          >
-            Delete
-          </button> */}
           <Hits hitComponent={Hit} />
         </InstantSearch>
       </div>
-      {/* ); })} */}
     </div>
   );
 }
