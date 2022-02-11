61abfa2044f00a2c897267f7aeea9d566e534779
angelica <angelica@amsbbgroup.com>
2022-02-11 10:27:22 +0800

    error

--- src/App.js
+++ src/App.js
@@ -1,49 +1,55 @@
-import { useState, useEffect } from "react";
+import { useState, Fragment, useEffect, useRef, createElement } from "react";
 import "./App.css";
-import { db } from "./firebase-config";
-import {
-  collection,
-  getDocs,
-  addDoc,
-  updateDoc,
-  doc,
-  deleteDoc,
-} from "firebase/firestore";
 import algoliasearch from "algoliasearch";
 import { InstantSearch, SearchBox, Hits } from "react-instantsearch-dom";
+import "@algolia/autocomplete-theme-classic";
+import { render } from "react-dom";
+import { Autocomplete } from "./components/boiler-auto";
+import { getAlgoliaResults } from "@algolia/autocomplete-js";
 
 const searchClient = algoliasearch(
   "3TS16BJ441",
   "2a6d438a38a33ec9f799981710c6eb18"
 );
 
-function App() {
-  const Hit = ({ hit }: { hit: events }) => {
-    return (
-      <section class="main">
-        <div class="card-container">
-          <div class="card-header">
-            <h4>{hit.eventName}</h4>
-            <p>{hit.eventInfo}</p>
-          </div>
-          <hr />
-        </div>
-      </section>
-    );
-  };
-
+export function EventItem({ hit, components }: { hit: events }) {
   return (
-    <div className="App">
-      <div>
-        <InstantSearch searchClient={searchClient} indexName="events">
-          <div className="search">
-            <SearchBox />
-          </div>
-          <br />
-          <Hits hitComponent={Hit} />
-        </InstantSearch>
+    <a href={hit.eventName} className="aa-ItemLink">
+      <div className="aa-ItemContent">
+        <div className="aa-ItemTitle">
+          <components.Highlight hit={hit} attribute="eventName" />
+        </div>
       </div>
-    </div>
+    </a>
+  );
+}
+
+function App() {
+  return (
+    <Autocomplete
+      openOnFocus={true}
+      getSources={({ events_query_suggestions }) => [
+        {
+          sourceId: "zTRewfs6DgJNfQTBYXkd",
+          getItems() {
+            return getAlgoliaResults({
+              searchClient,
+              queries: [
+                {
+                  indexName: "events",
+                  events_query_suggestions,
+                },
+              ],
+            });
+          },
+          templates: {
+            item({ item, components }) {
+              return <EventItem hit={item.eventName} components={components} />;
+            },
+          },
+        },
+      ]}
+    />
   );
 }
 
