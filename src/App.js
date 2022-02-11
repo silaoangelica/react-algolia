acb8bb62db5d36d0e035781ad49101d497386d1c
angelica <angelica@amsbbgroup.com>
2022-02-11 09:30:21 +0800

    Revert "Merge pull request #1 from silaoangelica/autocomplete-v1"
    This reverts commit 0ad2b92b543043d025475696da77ee1eec934341, reversing
    changes made to e52c7517d1d9d56d6f9dbf3630883f1adc0c1d10.

--- src/App.js
+++ src/App.js
@@ -1,61 +1,48 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import { db } from "./firebase-config";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
 import algoliasearch from "algoliasearch";
 import { InstantSearch, SearchBox, Hits } from "react-instantsearch-dom";
-import { Autocomplete } from "./components/boiler-algolia";
-import { getAlgoliaResults } from "@algolia/autocomplete-js";
-import { createAutocomplete } from "@algolia/autocomplete-core";
 
 const searchClient = algoliasearch(
   "3TS16BJ441",
   "2a6d438a38a33ec9f799981710c6eb18"
 );
 
-export function EventItem({ hit, components }) {
-  return (
-    <a href={hit.url} className="aa-ItemLink">
-      <div className="aa-ItemContent">
-        <div className="aa-ItemTitle">
-          <components.Highlight hit={hit} attribute="eventName" />
-        </div>
-      </div>
-    </a>
-  );
-}
-
 function App() {
-  // const Hit = ({ hit }: { hit: events }) => {
+  const Hit = ({ hit }: { hit: events }) => {
+    return (
+      <section class="main">
+        <div class="card-container">
+          <div class="card-header">
+            <h4>{hit.eventName}</h4>
+            <p>{hit.eventInfo}</p>
+          </div>
+          <hr />
+        </div>
+      </section>
+    );
+  };
+
   return (
-    <div className="app-container">
-      <h1>React Application</h1>
-      <Autocomplete
-        openOnFocus={true}
-        getSources={({ events_query_suggestions }) => [
-          {
-            sourceId: "string",
-            getItems() {
-              return getAlgoliaResults({
-                searchClient,
-                queries: [
-                  {
-                    indexName: "events",
-                    events_query_suggestions,
-                  },
-                ],
-              });
-            },
-            templates: {
-              item({ item, components }) {
-                return (
-                  <>
-                    <EventItem hit={item} components={components} />
-                  </>
-                );
-              },
-            },
-          },
-        ]}
-      />
+    <div className="App">
+      <div>
+        <InstantSearch searchClient={searchClient} indexName="events">
+          <div className="search">
+            <SearchBox />
+          </div>
+          <br />
+          <Hits hitComponent={Hit} />
+        </InstantSearch>
+      </div>
     </div>
   );
 }
