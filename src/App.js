2cfe4f486e60c0c6848c4f7665752dfb30938b38
angelica <angelica@amsbbgroup.com>
2022-02-14 16:49:16 +0800

    workinggggggg

--- src/App.js
+++ src/App.js
@@ -1,61 +1,99 @@
 import { useState, Fragment, useEffect, useRef, createElement } from "react";
 import "./App.css";
 import algoliasearch from "algoliasearch";
-import { InstantSearch, SearchBox, Hits } from "react-instantsearch-dom";
+import {
+  InstantSearch,
+  SearchBox,
+  Hits,
+  HierarchicalMenu,
+  Menu,
+  Pagination,
+  PoweredBy,
+  RatingMenu,
+  RefinementList,
+  ClearRefinements,
+} from "react-instantsearch-dom";
 import "@algolia/autocomplete-theme-classic";
 import { render } from "react-dom";
 import { Autocomplete } from "./components/boiler-auto";
 import { getAlgoliaResults } from "@algolia/autocomplete-js";
+import qs from "qs";
+import { useLocation, useNavigate } from "react-router-dom";
+import React from "react";
+import Highlight from "react-instantsearch-dom/dist/cjs/widgets/Highlight";
 
 const searchClient = algoliasearch(
-  "3TS16BJ441",
-  "2a6d438a38a33ec9f799981710c6eb18"
+  "latency",
+  "6be0576ff61c053d5f9a3225e2a90f76"
 );
 
-export function EventItem({ hit, components }) {
-  return (
-    <a href={hit.url} className="aa-ItemLink">
-      <div className="aa-ItemContent">
-        <div className="aa-ItemTitle">
-          <components.Highlight hit={hit} attribute="eventName" />
-        </div>
-      </div>
-    </a>
-  );
-}
+const DEBOUNCE_TIME = 700;
+const createURL = (state) => `?${qs.stringify(state)}`;
+const searchStateToUrl = (location, searchState) =>
+  searchState ? `${location.pathname}${createURL(searchState)}` : "";
+const urlToSearchState = (location) => qs.parse(location.search.slice(1));
 
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [searchState, setSearchState] = React.useState(
+    urlToSearchState(location)
+  );
+  const setStateId = React.useRef();
+
+  React.useEffect(() => {
+    const nextSearchState = urlToSearchState(location);
+
+    if (JSON.stringify(searchState) !== JSON.stringify(nextSearchState)) {
+      setSearchState(nextSearchState);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
+
+  function onSearchStateChange(nextSearchState) {
+    clearTimeout(setStateId.current);
+
+    setStateId.current = setTimeout(() => {
+      navigate(searchStateToUrl(location, nextSearchState), nextSearchState);
+    }, DEBOUNCE_TIME);
+
+    setSearchState(nextSearchState);
+  }
   return (
-    <div className="app-container">
-      <h1>React Application</h1>
-      <Autocomplete
-        openOnFocus={true}
-        getSources={({ query }) => [
-          {
-            sourceId: "events",
-            getItems() {
-              return getAlgoliaResults({
-                searchClient,
-                queries: [
-                  {
-                    indexName: "events",
-                    query,
-                    params: {
-                      hitsPerPage: 5,
-                    },
-                  },
-                ],
-              });
-            },
-            templates: {
-              item({ item, components }) {
-                return <EventItem hit={item} components={components} />;
-              },
-            },
-          },
-        ]}
-      />
-    </div>
+    <InstantSearch
+      searchClient={searchClient}
+      indexName="instant_search"
+      searchState={searchState}
+      onSearchStateChange={onSearchStateChange}
+      createURL={createURL}
+    >
+      <div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            marginBottom: 10,
+          }}
+        >
+          <SearchBox />
+        </div>
+        <div style={{ display: "flex" }}>
+          <div style={{ display: "flex", flexDirection: "column", flex: 1 }}>
+            <div style={{ display: "flex", justifyContent: "space-around" }}>
+              <ClearRefinements />
+            </div>
+            <div>
+              <Hits />
+            </div>
+            <div style={{ alignSelf: "center" }}>
+              <Pagination showLast={true} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </InstantSearch>
   );
 }
 
